921c28b31bf75929217ad93091204d5a40e89aae
ronshien <ronshien@gmail.com>
2019-12-01 18:10:38 -0500

    signin

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignupPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignupPage}></Route>
       </Switch>
     </div>
   );
