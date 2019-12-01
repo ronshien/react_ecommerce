a1e573994607de187ed8c2c552d8c3c58c0bf312
ronshien <ronshien@gmail.com>
2019-11-30 22:06:14 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = props => {
+  console.log(props);
+  return <div>HATS</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
